82ed853f7fd423d70e790ba9a474bdfe9165dc73
kenduraghav <kenduraghav@gmail.com>
2021-07-24 03:17:19 +0800

    Changed greetings message

--- src/main/java/sparkjava/tutorials/SparkJavaApplication.java
+++ src/main/java/sparkjava/tutorials/SparkJavaApplication.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class SparkJavaApplication {
 
 	public static void main(String[] args) {
-		get("/hello-world", (req,res) -> "Runtime changing. Say Hello to Spark Java. A micro framework");
+		get("/hello-world", (req,res) -> "Say Hello to Spark Java. A micro framework");
 	}
 
 }
